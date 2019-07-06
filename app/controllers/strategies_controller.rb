4b130759960d740efbe79f0fd8c7224f6104c883
Chow Jia Ying <chowjiaying211@gmail.com>
2019-07-07 06:55:00 +0800

    Modified Investment Strategies page to be information panel

--- app/controllers/strategies_controller.rb
+++ app/controllers/strategies_controller.rb
@@ -1,10 +1,9 @@
 class StrategiesController < ApplicationController
 	def index
-		@strategies=["Dividend", "Value"]
+		@strategies=[{:name => "Dividend Investing", :explanation => "Looks at how much dividends a stock gives."}, 
+					{:name => "Value Investing", :explanation => "Looks at intrinsic value of stock."}]
 	end
 
 	def show
-		@companies = [{:company => "Company 1", :result => "PASS"}, 
-					{:company => "Company 2", :result => "FAIL"}]
 	end
 end
