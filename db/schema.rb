7e9347a4f12c4d347c83e7196db0a1913c00646b
TheRealRavager <iskandar@u.nus.edu>
2019-07-06 18:49:29 +0800

    Create indicator models

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20190706104345) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "indicators", force: :cascade do |t|
+    t.string "indicator_type"
+    t.float "value"
+    t.datetime "last_updated"
+    t.integer "company_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
