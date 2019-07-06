db5dd4ee4c98b93106efc23318e90668da1b4d13
TheRealRavager <iskandar@u.nus.edu>
2019-07-06 22:23:39 +0800

    Remove relation between Indicator and IndicatorType

--- app/models/indicator.rb
+++ app/models/indicator.rb
@@ -1,4 +1,3 @@
 class Indicator < ApplicationRecord
     belongs_to :company, class_name: 'Company', foreign_key: 'company_id'
-    belongs_to :indicator_type, class_name: 'IndicatorType', foreign_key: 'type'
 end
