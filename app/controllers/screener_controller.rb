16b26ffd1f1f789c0c8c3d11b2ed0223d2bac63a
TheRealRavager <iskandar@u.nus.edu>
2019-07-07 12:57:22 +0800

    Edit screener indicators page

--- app/controllers/screener_controller.rb
+++ app/controllers/screener_controller.rb
@@ -13,32 +13,43 @@ class ScreenerController < ApplicationController
     @strategy_info = ["Dividend Investing", "Value Investing"][strategy]
     @company_pass_fail = []
     Company.all.each do |company|
-      @company_pass_fail << {company: company, pass: screen_company(@strategy, company)}
+      @company_pass_fail << {company: company, pass: screen_company(strategy, company)}
     end
   end
 
   def indicators
+    @thresholds = {"P/E Ratio" => "< 20", "Current Ratio" => "> 1.5", "D/E Ratio" => "< 0.35"}
+    strategy = params[:strategy_id].to_i
     @company = Company.find(params[:company_id])
     @indicators_pass_fail = []
     @company.indicators.all.each do |indicator|
-      @indicators_pass_fail << {name: indicator.category,
-        pass: (case indicator.category
-          when 'P/E Ratio'
-            price_to_earnings_indicator_test(indicator)
-          when 'Current Ratio'
-            current_ratio_indicator_test(indicator)
-          when 'D/E Ratio'
-            debt_to_equity_indicator_test(indicator)
-          end
-        )}
+      if strategy == 0
+        @indicators_pass_fail << {name: indicator.category,
+          pass: (case indicator.category
+            when 'P/E Ratio'
+              price_to_earnings_indicator_test(indicator)
+            end
+          )}
+      elsif strategy == 2 || strategy == 1
+        @indicators_pass_fail << {name: indicator.category,
+          pass: (case indicator.category
+            when 'P/E Ratio'
+              price_to_earnings_indicator_test(indicator)
+            when 'Current Ratio'
+              current_ratio_indicator_test(indicator)
+            when 'D/E Ratio'
+              debt_to_equity_indicator_test(indicator)
+            end
+          )}
+      end
     end
   end
 
   # Will perform each test differently / diff combination of test depending on strategy type
   def screen_company(strategy, company)
-    if strategy == "Dividend Investing"
+    if strategy == 0
       price_to_earnings_company_test(company) 
-    else 
+    elsif strategy == 2 || strategy == 1
       price_to_earnings_company_test(company) && current_ratio_company_test(company) && debt_to_equity_company_test(company)
     end
   end
