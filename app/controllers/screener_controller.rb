efc3ddccf51c7f9a6dbbbc8a6c7fdaf312029fb7
TheRealRavager <iskandar@u.nus.edu>
2019-07-07 12:04:11 +0800

    Make various UI/UX changes
    * Change bulma overview middle portion to small
    * Update stock screener tab and all associated views except indicators

--- app/controllers/screener_controller.rb
+++ app/controllers/screener_controller.rb
@@ -1,8 +1,9 @@
 class ScreenerController < ApplicationController
   def strategies
-    @strategies=["Dividend", "Value"]
-    # To be uncommented when strategy models created
-    # @Strategies = Strategy.all
+    @count = 0
+	  @strategies = [
+	    {:name => "Dividend Investing", :explanation => "Dividend investing is where investors buy stocks and collect dividends, which are a portion of a company’s earnings distributed to the shareholders."}, 
+	    {:name => "Value Investing", :explanation => "Value investing is an investing strategy that involves picking stocks that appear to be trading for less than their intrinsic or book value then selling them for a price that is higher than their original cost."}]
   end
 
   def companies
