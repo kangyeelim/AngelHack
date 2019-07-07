452e3ae2e1a5eeec09d9886d12e66cc9d8bee7eb
Felicia Tay <E0323765@u.nus.edu>
2019-07-07 12:18:31 +0800

    Edited @strategies array

--- app/controllers/screener_controller.rb
+++ app/controllers/screener_controller.rb
@@ -1,14 +1,13 @@
 class ScreenerController < ApplicationController
   def strategies
-    @count = 0
-	  @strategies = [
-	    {:name => "Dividend Investing", :explanation => "Dividend investing is where investors buy stocks and collect dividends, which are a portion of a company’s earnings distributed to the shareholders."}, 
-	    {:name => "Value Investing", :explanation => "Value investing is an investing strategy that involves picking stocks that appear to be trading for less than their intrinsic or book value then selling them for a price that is higher than their original cost."}]
+    @strategies=["Dividend Investing", "Value Investing"]
+    # To be uncommented when strategy models created
+    # @Strategies = Strategy.all
   end
 
   def companies
     strategy = params[:strategy_id].to_i
-    @strategy_info = ["Dividend", "Value"][strategy]
+    @strategy_info = ["Dividend Investing", "Value Investing"][strategy]
     @company_pass_fail = []
     Company.all.each do |company|
       @company_pass_fail << {company: company, pass: screen_company(@strategy, company)}
--- app/models/indicator_type.rb
+++
@@ -1,3 +0,0 @@
-class IndicatorType < ApplicationRecord
-    has_many :indicators, class_name: 'Indicator', foreign_key: 'type'
-end
